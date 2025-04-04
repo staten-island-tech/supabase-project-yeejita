d9792c0dcdc71fa6a4b27ac7367adfba97daf826
dashcaj <158211825+dashcaj@users.noreply.github.com>
2025-04-04 13:59:46 -0400

    w

---
+++ app/supabaseClient.js
@@ -0,0 +1,2 @@
+import { createClient } from '@supabase/supabase-js'
+  export const supabase = createClient('https://tconkirobtwwxjgojmie.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRjb25raXJvYnR3d3hqZ29qbWllIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDMwMTE1ODcsImV4cCI6MjA1ODU4NzU4N30.35X0ztuOCOXvXR3z7Lx_-dh72DyNY-cEqjuT6XMJ6Jg')
