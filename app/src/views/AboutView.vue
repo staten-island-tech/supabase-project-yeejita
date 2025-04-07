6734b201c842353dc72a01e32c4c853a8ba53ddb
Benjamin Yee <benyee1209@gmail.com>
2025-04-07 13:39:56 -0400

    BOOM!

--- app/src/views/AboutView.vue
+++ app/src/views/AboutView.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+ <h1 class="text-orange-800">
+    Hello world!
+  </h1>
 </template>
 
 <style>
--- app/vite.config.js
+++ app/vite.config.js
@@ -1,12 +1,13 @@
 import { fileURLToPath, URL } from 'node:url'
-
+import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
+  plugins: [ 
+    tailwindcss(),
     vue(),
     vueDevTools(),
   ],
