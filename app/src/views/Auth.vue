3d4bad94c50ef706948cd1a1528ee510002a1dff
dashcaj <158211825+dashcaj@users.noreply.github.com>
2025-04-22 13:59:32 -0400

    gfjfghfghjfghjfghjfghj

---
+++ app/src/views/Auth.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="register">
+    <h2>signup</h2>
+    <form @submit.prevent="registerUser">
+      <div>
+        <label for="username">Username</label>
+        <input type="text" id="username" v-model="username" required />
+      </div>
+      <div>
+        <label for="email">Email</label>
+        <input type="email" id="email" v-model="email" required />
+      </div>
+      <div>
+        <label for="password">Password</label>
+        <input type="password" id="password" v-model="password" required />
+      </div>
+      <button type="submit">sign up?</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import { ref, reactive } from "vue";
+
+
+</script>
