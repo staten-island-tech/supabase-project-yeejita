7ad2512b4bf434686f9aa8ec71b18890afdc350c
Benjamin Yee <benyee1209@gmail.com>
2025-04-22 13:59:52 -0400

    asd
    asd

--- app/src/components/TheWelcome.vue
+++ app/src/components/TheWelcome.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-<h1>Welcome to SupaFoodBase</h1>
+<h1 class = "text-center text-4xl ">Welcome to SupaFoodBase</h1>
 <h2>Sign in and create and share recipes</h2>
 <h2>View and rate other users' posts</h2>
   </div>
--- app/src/components/icons/PublicPage.vue
+++ app/src/components/icons/PublicPage.vue
@@ -1,10 +1,23 @@
 <template>
     <div>
         <h1>View other user posts:</h1>
-        <button  class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 border border-greem-700 rounded">Recent</button>
-        <button  class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 border border-green-700 rounded">Top</button>
-        <button  class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 border border-green-700 rounded">Oldest</button>
+        <button  class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-8 border border-greem-700 rounded">Recent</button>
+        <button  class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-8 border border-green-700 rounded">Top</button>
+        <button  class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-8 border border-green-700 rounded">Oldest</button>
     </div>
+
+    <div class="relative text-center">
+  <select class="...">
+    <option>Recent</option>
+    <option>Top</option>
+    <option>Oldest</option>
+  </select>
+  
+</div>
+
+<div>
+    <input type="text" name="sugoi" id="">
+</div>
 </template>
 
 <script setup>
