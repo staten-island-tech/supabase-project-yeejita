30139495f8551c80430bf1a80f8db184a9b2772c
Benjamin Yee <benyee1209@gmail.com>
2025-05-12 13:52:16 -0400

    das

--- app/src/components/icons/PublicPage.vue
+++ app/src/components/icons/PublicPage.vue
@@ -21,8 +21,7 @@
   <div class = "posts">
 
     <div class = "post, flex-wrap	">
-      <button class = "bg-transparent hover:bg-green-500 text-green-700 font-semibold hover:text-white py-2 px-4 border border-green-500 hover:border-transparent rounded-lg">👍 </button>
-      <button class = "bg-transparent hover:bg-red-500 text-red-700 font-semibold hover:text-white py-2 px-4 border border-red-500 hover:border-transparent rounded-lg">👎 </button>
+      
     </div>
 
 
@@ -32,6 +31,10 @@
 
 <script setup>
 //insert text into textbox ad submit to show on screen
+/* insertL 
+<button class = "bg-transparent hover:bg-green-500 text-green-700 font-semibold hover:text-white py-2 px-4 border border-green-500 hover:border-transparent rounded-lg">👍 </button>
+      <button class = "bg-transparent hover:bg-red-500 text-red-700 font-semibold hover:text-white py-2 px-4 border border-red-500 hover:border-transparent rounded-lg">👎 </button>
+      */
 </script>
 
 <style scoped>
