641af1a27535c5e0244a57d972f374916b9a57ee
Benjamin Yee <benyee1209@gmail.com>
2025-05-27 13:44:06 -0400

    asd

--- app/src/components/icons/PostForm.vue
+++ app/src/components/icons/PostForm.vue
@@ -22,8 +22,11 @@
         class="p-4 bg-gray-100 rounded shadow relative"
       >
         <h2 class="font-bold text-lg mb-2">{user.name}</h2>
-        <p class="whitespace-pre-line">{{ post }}</p>
-
+        <p class="whitespace-pre-line m-8">{{ post }}</p>  
+        <div class = "mt-6"><button class = "bg-transparent hover:bg-green-500 text-green-700 font-semibold hover:text-white py-2 px-4 border border-green-500 hover:border-transparent rounded-lg">👍 </button>
+      <button class = "bg-transparent hover:bg-red-500 text-red-700 font-semibold hover:text-white py-2 px-4 border border-red-500 hover:border-transparent rounded-lg">👎 </button>
+</div>
+        
         <!-- Delete Button -->
         <button
           @click="deletePost(index)"
--- app/src/components/icons/userpost.vue
+++ app/src/components/icons/userpost.vue
@@ -1,7 +1,6 @@
 <template>
     <div class = "flex flex-wrap">
-        <h1>this is a placeholder text title</h1>
-        <h2>this is place holder body paragraph for post </h2>
+        
         <button class = "bg-transparent hover:bg-green-500 text-green-700 font-semibold hover:text-white py-2 px-4 border border-green-500 hover:border-transparent rounded-lg">👍 </button>
       <button class = "bg-transparent hover:bg-red-500 text-red-700 font-semibold hover:text-white py-2 px-4 border border-red-500 hover:border-transparent rounded-lg">👎 </button>
     </div>
