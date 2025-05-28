2f0766942a9bd65befdc1c1ecd1dadf953a32923
Benjamin Yee <benyee1209@gmail.com>
2025-05-28 13:25:32 -0400

    wqe

--- app/src/components/icons/PublicPage.vue
+++ app/src/components/icons/PublicPage.vue
@@ -24,10 +24,8 @@
 
 <script setup>
 //insert text into textbox ad submit to show on screen
-/* insertL 
-<button class = "bg-transparent hover:bg-green-500 text-green-700 font-semibold hover:text-white py-2 px-4 border border-green-500 hover:border-transparent rounded-lg">👍 </button>
-      <button class = "bg-transparent hover:bg-red-500 text-red-700 font-semibold hover:text-white py-2 px-4 border border-red-500 hover:border-transparent rounded-lg">👎 </button>
-      */
+
+
 
 import allposts from './allposts.vue';
 </script>
